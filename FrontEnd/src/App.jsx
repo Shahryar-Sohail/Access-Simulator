683dacbc0bbdb693f0622ff23fc97e43ed10eed3
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-09-22 02:41:20 +0500

    Ui completed

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -6,14 +6,15 @@ import './index.css'
 function App() {
 
   const [employees, setEmployees] = useState([])
+  const [response, setResponse] = useState(null);
 
   useEffect(() => {
     const fetchEmployees = async () => {
       try {
         const res = await fetch("http://localhost:3000/employees");
-        if (!res.ok){
-            throw new Error("Network response was not ok");
-          }
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
         const data = await res.json();
         setEmployees(data);
       } catch (err) {
@@ -24,6 +25,19 @@ function App() {
     fetchEmployees();
   }, []);
 
+  async function simulateAccess() {
+    const res = await fetch("http://localhost:3000/simulate");
+    const data = await res.json();
+    setResponse(data);
+    console.log(data);
+  }
+  async function refresh() {
+    const res = await fetch("http://localhost:3000/employees");
+    const data = await res.json();
+    setEmployees(data);
+    setResponse(null);
+  }
+
 
   return (
     <div className='bg-gradient-to-t from-slate-100 via-slate-300 to-slate-100 w-full min-h-screen'>
@@ -31,7 +45,12 @@ function App() {
 
       {/* Employees Table */}
       <div className='border w-5/6 mx-auto p-5 rounded-lg shadow-lg'>
-        <div className="overflow-x-auto">
+        <div className='flex justify-center gap-4 p-4'>
+          <button onClick={() => simulateAccess(employees)} className="btn btn-outline btn-error">Simulate Access</button>
+          <button onClick={() => refresh()} className="btn btn-outline btn-success">Refresh</button>
+        </div>
+
+        <div className="overflow-x-auto flex">
           <table className="table">
             {/* head */}
             <thead>
@@ -39,9 +58,7 @@ function App() {
                 <th>Id</th>
                 <th>Access Level</th>
                 <th>Request Time</th>
-                <th>Cool Down</th>
                 <th>Room</th>
-                <th>Access</th>
               </tr>
             </thead>
             <tbody>
@@ -52,14 +69,12 @@ function App() {
                     <td>{employee.id}</td>
                     <td>{employee.access_level}</td>
                     <td>{employee.request_time}</td>
-                    <td>{employee.cool_down || 0}</td>
                     <td>{employee.room}</td>
-                    <th>
-                      <button onClick={()=> alert(`Simulating access for ${employee.id}`)} className="btn btn-outline btn-error">Simulate Access</button>
-                    </th>
                   </tr>
                 ))
               }
+
+
 
 
             </tbody>
@@ -69,13 +84,50 @@ function App() {
                 <th>Id</th>
                 <th>Access Level</th>
                 <th>Request Time</th>
-                <th>Cool Down</th>
                 <th>Room</th>
-                <th>Access</th>
               </tr>
             </tfoot>
           </table>
+
+
+          {response &&
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+
+                  <th>Status</th>
+                  <th>Message</th>
+
+                </tr>
+              </thead>
+              <tbody>
+
+                {
+                  response.map((res, index) => (
+                    <tr key={`${res.id}-${index}`}>
+                      <td>{res.status}</td>
+                      <td>{res.message}</td>
+                    </tr>
+                  ))
+                }
+
+
+
+
+              </tbody>
+              {/* foot */}
+              <tfoot>
+                <tr>
+                  <th>Status</th>
+                  <th>Message</th>
+                </tr>
+              </tfoot>
+            </table>
+          }
+
         </div>
+
       </div>
 
 
